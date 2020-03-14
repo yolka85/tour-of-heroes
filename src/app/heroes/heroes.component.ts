b6b1efabef65e547541ef7b35a885320e684a853
Yolka85 <4yolka@gmail.com>
2020-03-14 22:27:47 +0200

    Added add, edit, delete hero, search, nav routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,23 +9,30 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-
   heroes: Hero[];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
